f8c8ec520df3139d1d55bf82d7c610fad0eacb61
Bimini Harsha Tarun <2200030868@kluniversity.in>
2024-03-05 15:19:54 +0530

    commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1 className='text-bold'>Hello, Welcome to Realestate Management system</h1>
+      <h1>Hello, Welcome to Realestate Management system</h1>
     </div>
   )
 }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -129,6 +129,24 @@ const Profile = () => {
       setShowListingsError(true);
     }
   };
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -224,6 +242,14 @@ const Profile = () => {
                   className='h-16 w-16 object-contain'
                 />
               </Link>
+              <Link className='text-slate-700 font-semibold hover:underline truncate flex-1' to ={`/listing/${listing._id}`}>
+                <p>{listing.name}</p>
+              </Link>
+              <div className='flex flex-col item-center'>
+                <button onClick={()=>handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
+                <button className='text-green-700 uppercase'>Edit</button>
+                </div>
+
             </div>
           ))}
         </div>
