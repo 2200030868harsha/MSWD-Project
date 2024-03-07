3856205590add12f5f77b17f5426feeaa96fff6c
Bimini Harsha Tarun <2200030868@kluniversity.in>
2024-03-07 22:15:04 +0530

    commit

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -116,19 +116,22 @@ const Profile = () => {
       setShowListingsError(false);
       const res = await fetch(`/api/user/listings/${currentUser._id}`);
       const data = await res.json();
-      console.log('Listings API response:', data);
-
+  
       if (data.success === false) {
         setShowListingsError(true);
         return;
       }
-
-      setUserListings(data.data); // Assuming data is an array of listings
+  
+      // Assuming data is an array of listings, adjust this part according to your API response
+      setUserListings(data.data || []);
     } catch (error) {
       console.error('Error fetching listings:', error);
       setShowListingsError(true);
     }
   };
+  
+
+  
   const handleListingDelete = async (listingId) => {
     try {
       const res = await fetch(`/api/listing/delete/${listingId}`, {
@@ -236,18 +239,24 @@ const Profile = () => {
           {userListings.map((listing) => (
             <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4'>
               <Link to={`/listing/${listing._id}`}>
-                <img
-                  src={listing.imageUrls[0]} // Assuming imageUrls is an array
-                  alt='listing cover'
-                  className='h-16 w-16 object-contain'
-                />
+              {listing.imageUrls.map((imageUrl, index) => (
+              <img
+              key={index}
+              src={imageUrl}
+              alt={`listing cover ${index}`}
+              className='h-16 w-16 object-contain'
+            />
+            ))}
+
               </Link>
               <Link className='text-slate-700 font-semibold hover:underline truncate flex-1' to ={`/listing/${listing._id}`}>
                 <p>{listing.name}</p>
               </Link>
               <div className='flex flex-col item-center'>
                 <button onClick={()=>handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
-                <button className='text-green-700 uppercase'>Edit</button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className='text-green-700 uppercase'>Edit</button>
+                </Link>
                 </div>
 
             </div>
