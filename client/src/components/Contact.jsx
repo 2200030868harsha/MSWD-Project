438e6a81ecd47bda9d3e62d92fc11d20b3c74e8e
Bimini Harsha Tarun <2200030868@kluniversity.in>
2024-04-25 21:59:36 +0530

    hi

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -39,12 +39,13 @@ export default function Contact({ listing }) {
             className='w-full border p-3 rounded-lg'
           ></textarea>
 
-          <Link
-          to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
-          className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95'
-          >
-            Send Message          
-          </Link>
+<Link
+  to={`mailto:${encodeURIComponent(landlord.email)}?subject=${encodeURIComponent(`Regarding ${listing.name}`)}&body=${encodeURIComponent(message)}`}
+  className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95'
+>
+  Send Message          
+</Link>
+
         </div>
       )}
     </>
